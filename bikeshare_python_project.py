c472278d9a078c00d1a3742f7a7c44156e927c55
Reem Mahrougi <rmahrougi@JODC.COM.SA>
2019-12-10 21:18:12 +0300

    change in print statement in trip_duration_stat function

--- bikeshare_python_project.py
+++ bikeshare_python_project.py
@@ -10,12 +10,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 def get_filters():
     
     """
-    Asks user to specify a city, month, and day to analyze.
-
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    This function gets the user input for a city, month, and day to analyze.
+        It returns the city,day,and month to be called in other functions  
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # Get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
@@ -127,7 +123,7 @@ def station_stats(df):
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
-    print('\nCalculating Trip Duration...\n')
+    print('\nCalculating Trip Duration statistics...\n')
     start_time = time.time()
 
     # displays total travel time
