e52d1de6f7a64dd7bb9f88173c96bd3a9f086e7e
Reem Mahrougi <rmahrougi@JODC.COM.SA>
2019-12-13 11:52:31 +0300

    refactor: remove print calculating user stats in user_stats function

--- bikeshare_python_project.py
+++ bikeshare_python_project.py
@@ -140,8 +140,6 @@ def trip_duration_stats(df):
 def user_stats(df):
     """Displays statistics on bikeshare users."""
 
-    print('\nCalculating User Stats...\n')
-    start_time = time.time()
 
     # Displays counts of user types
     totalsubscribers = df['User Type'].str.count('Subscriber').sum()
